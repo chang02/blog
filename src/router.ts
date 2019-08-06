f53ec39e1dcf7fa359e9d82afa862208a19c28a9
cy <chang0022@snu.ac.kr>
2019-08-06 16:24:59 +0900

    redirect to profile

--- src/router.ts
+++ src/router.ts
@@ -8,6 +8,11 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
+      path: '/',
+      name: 'home',
+      component: () => import('./views/Home.vue'),
+    },
+    {
       path: '/profile',
       name: 'profile',
       component: () => import('./views/Profile.vue'),
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-layout>
+  </v-layout>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component<Home>({})
+export default class Home extends Vue {
+  public created() {
+    this.$router.push('/profile');
+  }
+}
+</script>
+<style scoped>
+</style>
