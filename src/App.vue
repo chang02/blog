7cbd96e84465e0ec4eef73d21a208c3778d21219
cy <chang0022@snu.ac.kr>
2019-08-12 17:13:52 +0900

    router -> viewMode

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
       v-model="drawer" app clipped width="200"
     >
       <v-list dense>
-        <v-list-item @click="$router.push('/profile')" :class="$router.currentRoute.path==='/profile'?'purple lighten-3':''">
+        <v-list-item @click="viewMode='profile'" :class="viewMode==='profile'?'purple lighten-3':''">
           <v-list-item-action>
             <v-icon>mdi-account</v-icon>
           </v-list-item-action>
@@ -18,7 +18,7 @@
             <v-list-item-title>Profile</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item @click="$router.push('/resume')" :class="$router.currentRoute.path==='/resume'?'purple lighten-3':''">
+        <v-list-item @click="viewMode='resume'" :class="viewMode==='resume'?'purple lighten-3':''">
           <v-list-item-action>
             <v-icon>mdi-file-document-box</v-icon>
           </v-list-item-action>
@@ -29,16 +29,25 @@
       </v-list>
     </v-navigation-drawer>
     <v-content>
-      <router-view />
+      <profile v-if="viewMode === 'profile'" />
+      <resume v-if="viewMode === 'resume'" />
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import Profile from '@/views/Profile.vue';
+import Resume from '@/views/Resume.vue';
 
-@Component<App>({})
+@Component<App>({
+  components: {
+    Profile,
+    Resume,
+  },
+})
 export default class App extends Vue {
   private drawer: boolean = true;
+  private viewMode: string = 'profile';
 }
 </script>
